53f2a3e79b0b960dace1d0b04da64ff2bb45860a
jcovian <jose@covian.es>
2022-05-16 17:35:03 +0200

    Mod2T6S49_Fin

---
+++ Módulo 2/Tema 6_React/cheatsheet/my-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola, mundo</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, tempore?</p>
+      <FirstComponent></FirstComponent>
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;

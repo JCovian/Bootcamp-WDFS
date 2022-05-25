db2421b9d50856bf6c45844ad93f6e8e048cda9a
jcovian <jose@covian.es>
2022-05-25 22:54:42 +0200

    Mod2T6S52_Fin

---
+++ Módulo 2/Tema 6_React/ejercicios/ejercicio3/src/components/Agenda.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Agenda({ contacts }) {
+  
+  return (
+    <div className='row'>
+      {contacts.map((contact, index) => {
+        return (
+          <ul className='list-group mb-3 col-12 col-sm-6 col-xl-3' key={contact.phoneNumber}>
+            <li className='list-group-item active'>Contacto {index + 1}</li>
+            <li className='list-group-item'>{contact.name}</li>
+            <li className='list-group-item'>{contact.lastName}</li>
+            <li className='list-group-item'>{contact.phoneNumber}</li>
+            <li className='list-group-item'>{contact.address}, {contact.postalCode}, {contact.city}</li>           
+          </ul>
+        )
+      })}
+
+    </div>
+  )
+}
